48b69e7f49644bab6fe2599cfb644b5aae60a22c
zabdulz <testingmyways@gmail.com>
2019-11-08 12:28:03 +0000

    Code Churn Analysis

--- countComments.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#searches for all .java files in the directory and sub-directories and count the number of lines of comments of code in the file
-
-for file in `find . -name '*.java'`
-	do
-    total=$(grep "\/\*" $file | wc -l)
-    echo "$file,$total"
-done
--- countVocab.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#searches for all .java files in the directory and sub-directories and count the number of vocabularies in the file
-
-for file in `find . -name '*.java'`
-	do
-    total=$(grep -roE '\w+' $file | sort -u -f | wc -l)
-    echo "$file,$total"
-done
